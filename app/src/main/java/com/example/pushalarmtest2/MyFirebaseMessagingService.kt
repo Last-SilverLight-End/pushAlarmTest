8b139bb798c7e2981b394654b97a232d6c8e9429
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-08-19 08:52:20 +0900

    making notification manager

--- app/src/main/java/com/example/pushalarmtest2/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/pushalarmtest2/MyFirebaseMessagingService.kt
@@ -1,9 +1,14 @@
 package com.example.pushalarmtest2
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
@@ -11,5 +16,32 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
+        createNotificationChannel()
+
+        NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
     }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    // static 과 비슷하나
+    companion object {
+        private const val CHANNEL_NAME = "Emoji party"
+        private const val CHANNEL_DESCRIPTION = "Emoji party 를 위한 채널"
+        private const val CHANNEL_ID = "Channel Id"
+    }
+
 }
